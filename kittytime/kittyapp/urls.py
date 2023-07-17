5b6d5189c1d3b07b1ab2339afcb1da86e803b436
almeidarayene <almeidarayene@gmail.com>
2023-07-17 16:39:51 -0300

    login

--- kittytime/kittyapp/urls.py
+++ kittytime/kittyapp/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup'), 
     path('user/list/', views.user_list, name='user_list'),
     path('user/create/', views.user_create, name='user_create'),
     path('user/update/<int:id>/', views.user_update, name='user_update'),
     path('user/delete/<int:id>/', views.user_delete, name='user_delete'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 ]
 
--- kittytime/kittyapp/views.py
+++ kittytime/kittyapp/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import CustomUser
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate
 
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirecionar para a página inicial após o login
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'login.html', {'form': form})
 
 def signup(request):
     if request.method == 'POST':
