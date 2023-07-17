9f4ab83262bf117de55c83da428cbe21bf3e05f2
almeidarayene <almeidarayene@gmail.com>
2023-07-17 16:06:00 -0300

    config

--- kittytime/kittyapp/models.py
+++ kittytime/kittyapp/models.py
@@ -1,15 +1,17 @@
-# kittyapp/models.py
-
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class CustomUser(AbstractUser):
-    # Campos personalizados podem ser adicionados aqui, se necessário
-    # Por exemplo, vamos adicionar um campo para o número de telefone:
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
 
-    # Se você deseja adicionar mais campos personalizados, basta adicionar mais linhas aqui.
+    groups = models.ManyToManyField(
+        'auth.Group',
+        blank=True,
+        related_name='custom_users'
+    )
 
-    def __str__(self):
-        return self.username
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        blank=True,
+        related_name='custom_users'
+    )
 
