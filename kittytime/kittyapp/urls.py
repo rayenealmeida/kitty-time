c8415b1b60b2bd7b6c1a9d3b642e7b5dd96bf324
almeidarayene <almeidarayene@gmail.com>
2023-07-20 11:21:09 -0300

    reset

--- kittytime/kittyapp/urls.py
+++ kittytime/kittyapp/urls.py
@@ -1,15 +1,10 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.user_login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('cats/', views.cat_list, name='cat_list'),  
-    path('cats/<int:cat_id>/', views.cat_detail, name='cat_detail'), 
-    path('cats/create/', views.cat_create, name='cat_create'),  
-    path('cats/<int:cat_id>/update/', views.cat_update, name='cat_update'),  
-    path('cats/<int:cat_id>/delete/', views.cat_delete, name='cat_delete'),  
 ]
