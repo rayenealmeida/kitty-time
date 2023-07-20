6da3dc784a1ea966d0885a9e7fedbe297dd6f4f3
almeidarayene <almeidarayene@gmail.com>
2023-07-20 15:53:22 -0300

    login

--- kittytime/kittyapp/urls.py
+++ kittytime/kittyapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('cadastro/', views.cadastro, name='cadastro'),
-    path('login/', views.login, name='login'),
-]
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
+    ]
