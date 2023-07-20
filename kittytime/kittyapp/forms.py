4834b525c3dd09a1e91d362827e890916f8b3ada
almeidarayene <almeidarayene@gmail.com>
2023-07-20 12:22:17 -0300

    AAAAAAA

--- kittytime/kittyapp/forms.py
+++ kittytime/kittyapp/forms.py
@@ -1,8 +1,9 @@
+# kittyapp/forms.py
 from django import forms
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Cat
-class CatForm(UserCreationForm):
+
+class CatForm(forms.ModelForm):  
     email = forms.EmailField()
 
     class Meta:
--- kittytime/kittyapp/urls.py
+++ kittytime/kittyapp/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.user_login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+
+    # Rotas relacionadas ao modelo Cat
+    path('cats/', views.cat_list, name='cat_list'),
+    path('cats/<int:cat_id>/', views.cat_detail, name='cat_detail'),
+    path('cats/create/', views.cat_create, name='cat_create'),
+    path('cats/<int:cat_id>/update/', views.cat_update, name='cat_update'),
+    path('cats/<int:cat_id>/delete/', views.cat_delete, name='cat_delete'),
 ]
