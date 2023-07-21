c73f116e69a64abc12c3da464503f0c7d0fbbe33
almeidarayene <almeidarayene@gmail.com>
2023-07-21 18:06:20 -0300

    CRUD initial

---
+++ kittytime/kittyapp/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Session
+
+class SessionForm(forms.ModelForm):
+    class Meta:
+        model = Session
+        fields = ['title', 'start_time', 'end_time']
--- kittytime/kittyapp/views.py
+++ kittytime/kittyapp/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from django.contrib.auth.models import User 
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_django
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
+from .models import Session
+from .forms import SessionForm
 
 def cadastro(request):
     if request.method == "GET":
@@ -42,3 +44,31 @@ def login(request):
 @login_required(login_url="/login/")
 def home(request):
     return render(request, 'home.html', {'user': request.user})
+
+#CRUD
+def list_sessions(request):
+    sessions = Session.objects.filter(user=request.user)
+    return render(request, 'list_sessions.html', {'sessions': sessions})
+
+def create_session(request):
+    if request.method == "POST":
+        form = SessionForm(request.POST)
+        if form.is_valid():
+            session = form.save(commit=False)
+            session.user = request.user
+            session.save()
+            return redirect('list_sessions')
+    else:
+        form = SessionForm()
+    return render(request, 'create_session.html', {'form': form})
+
+def update_session(request, session_id):
+    session = get_object_or_404(Session, id=session_id, user=request.user)
+    if request.method == "POST":
+        form = SessionForm(request.POST, instance=session)
+        if form.is_valid():
+            form.save()
+            return redirect('list_sessions')
+    else:
+        form = SessionForm(instance=session)
+    return render(request, 'update_session.html', {'form': form, 'session': session})
