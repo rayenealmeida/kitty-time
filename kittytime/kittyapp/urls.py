dc5ade134b5d028cce0929c327c823804fd0df41
almeidarayene <almeidarayene@gmail.com>
2023-07-21 22:19:31 -0300

    fim

--- kittytime/kittyapp/urls.py
+++ kittytime/kittyapp/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('home/', views.home, name='home'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),    
-    path('sessao/', views.sessao, name='sessao'),  # Atualize esta linha para 'sessao/'.
-    path('sessao/lista/', views.lista_sessoes, name='lista_sessoes'),  # Atualize esta linha para 'sessao/lista/'.
+   #CRUD
+    path('sessao/', views.sessao, name='sessao'),  
+    path('sessao/lista/', views.lista_sessoes, name='lista_sessoes'),  
     path('sessao/adicionar/', views.adicionar_sessao, name='adicionar_sessao'),
     path('sessao/atualizar/<int:sessao_id>/', views.atualizar_sessao, name='atualizar_sessao'),
     path('sessao/excluir/<int:sessao_id>/', views.excluir_sessao, name='excluir_sessao'),
